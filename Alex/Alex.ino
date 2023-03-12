e27d118367eb77c00e73b0d296e4afe0d6a91be2
haowern98 <111697767+haowern98@users.noreply.github.com>
2023-03-13 01:05:06 +0800

    Update Alex.ino

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -22,8 +22,8 @@ volatile TDirection dir = STOP;
 // Number of ticks per revolution from the 
 // wheel encoder.
 
-#define LEFT_COUNTS_PER_REV      400
-#define RIGHT_COUNTS_PER_REV 267
+#define LEFT_COUNTS_PER_REV      600
+#define RIGHT_COUNTS_PER_REV 400
 
 // Wheel circumference in cm.
 // We will use this to calculate forward/backward distance traveled 
